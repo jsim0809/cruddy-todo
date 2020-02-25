c171cb3a99e0832e8cb4368088b181c40afd7e7b
jsim0809 <jeremy_sim@hotmail.com>
2020-02-25 09:18:23 -0800

    Complete update

--- datastore/index.js
+++ datastore/index.js
@@ -53,13 +53,19 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  exports.readOne(id, (err, todoItem) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`), null);
+    } else {
+      fs.writeFile(path.join(exports.dataDir, id) + '.txt', text, (err) => {
+        if (err) {
+          throw ('error writing file');
+        } else {
+          callback(null, {id, text});
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
